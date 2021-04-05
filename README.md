85ca94c2b099154e149f43da4862fc96cd1f85fe
Fernando Batista <fernandohsbatista@gmail.com>
2021-04-05 18:13:38 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# langauage_detector
+
+This project means to be a place where I will test new stuff.
+
+## Topics covered by this project
+ - BLoC
---
+++ android/app/src/main/kotlin/com/example/langauage_detector/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.langauage_detector
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
