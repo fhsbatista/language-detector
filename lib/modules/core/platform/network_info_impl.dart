951d7ca14b972fdb3e763a2d5795ab5e4e7bac6b
Fernando Batista <fernandohsbatista@gmail.com>
2021-05-01 07:59:45 -0300

    Implementation of networkinfo

---
+++ lib/modules/core/platform/network_info_impl.dart
@@ -0,0 +1,13 @@
+import 'package:connectivity/connectivity.dart';
+import 'package:language_detector/modules/core/platform/network_info.dart';
+
+class NetworkInfoImpl implements NetworkInfo {
+  final Connectivity connectivity;
+
+  NetworkInfoImpl(this.connectivity);
+
+  @override
+  Future<bool> get isConnected async {
+    return await connectivity.checkConnectivity() != ConnectivityResult.none;
+  }
+}
---
+++ test/lib/modules/core/platform/network_info_impl_test.dart
@@ -0,0 +1,32 @@
+import 'package:connectivity/connectivity.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:language_detector/modules/core/platform/network_info_impl.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockConnectivity extends Mock implements Connectivity {}
+
+void main() {
+  late MockConnectivity connectivity;
+  late NetworkInfoImpl networkInfo;
+
+  setUp(() {
+    connectivity = MockConnectivity();
+    networkInfo = NetworkInfoImpl(connectivity);
+  });
+
+  group('isConnected', () {
+    test('should forward the call to the connectivity api', () async {
+      //arrange
+      final isConnected = true;
+      when(() => connectivity.checkConnectivity())
+          .thenAnswer((_) async => ConnectivityResult.mobile);
+
+      //act
+      final result = await networkInfo.isConnected;
+
+      //assert
+      verify(connectivity.checkConnectivity);
+      expect(result, isConnected);
+    });
+  });
+}
